f9f23163fdc9f73a09bbd765c0fbdcdcb00eac15
mamoreyra <mmoreyra@pigmalion.com.ar>
2023-04-30 12:00:40 -0300

    Add style to the invoice component

--- src/components/InvoiceApp.jsx
+++ src/components/InvoiceApp.jsx
@@ -6,45 +6,66 @@ function InvoiceApp() {
 
     return (
         <>
-            <h1>Factura</h1>
-            <ul>
-                <li>id: {invoice.id}</li>
-                <li>Name: {invoice.name}</li>
-            </ul>
-            <h3>Datos del Cliente</h3>
-            <ul>
-                <li>{invoice.client.name} {invoice.client.lastName}</li>
-                <li>{invoice.client.address.country}</li>
-                <li>{invoice.client.address.city}</li>
-                <li>{invoice.client.address.street} {invoice.client.address.number}</li>
-            </ul>
-            <h3>Datos de la empresa</h3>
-            <ul>
-                <li>{invoice.company.name}</li>
-                <li>{invoice.company.fiscalNumber}</li>
-            </ul>
-            <h4>Productos de la Factura</h4>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Producto</th>
-                        <th>Precio</th>
-                        <th>Cantidad</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {invoice.items.map(item => {
-                        return (
-                            <tr key = {item.id}>
-                                <td>{item.product}</td>
-                                <td>{item.price}</td>
-                                <td>{item.quantity}</td>
-                            </tr>
-                        )
-                    })}
+            <div className='container'>
+                <div className="card my-3">
+                    <div className="card-header">
+                        Factura
+                    </div>
 
-                </tbody>
-            </table>
+                    <div className="card-body">
+
+                        <ul className="list-group">
+                            <li className="list-group-item">id: {invoice.id}</li>
+                            <li className="list-group-item">Name: {invoice.name}</li>
+                        </ul>
+
+                        <div className="row my-3">
+                            <div className="col">
+                                <h3>Datos del Cliente</h3>
+                                <ul className="list-group">
+                                    <li className="list-group-item active">{invoice.client.name} {invoice.client.lastName}</li>
+                                    <li className="list-group-item">{invoice.client.address.country} {invoice.client.address.city}</li>
+                                    <li className="list-group-item">{invoice.client.address.street} {invoice.client.address.number}</li>
+                                </ul>
+                            </div>
+
+
+                            <div className="col" >
+                                <h3>Datos de la empresa</h3>
+                                <ul className="list-group">
+                                    <li className="list-group-item active">{invoice.company.name}</li>
+                                    <li className="list-group-item">{invoice.company.fiscalNumber}</li>
+                                </ul>
+                            </div>
+                        </div>
+
+
+                        <h4>Productos de la Factura</h4>
+                        <table className="table table-striped table-hover">
+                            <thead>
+                                <tr>
+                                    <th>Producto</th>
+                                    <th>Precio</th>
+                                    <th>Cantidad</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {invoice.items.map(item => {
+                                    return (
+                                        <tr key={item.id}>
+                                            <td>{item.product}</td>
+                                            <td>{item.price}</td>
+                                            <td>{item.quantity}</td>
+                                        </tr>
+                                    )
+                                })}
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+
         </>
     )
 }
