e1ba7ef362fa419d4d991d232946b9332dafe281
mamoreyra <mmoreyra@pigmalion.com.ar>
2023-04-30 15:36:04 -0300

    Add state in the diferents components

--- src/InvoiceApp.jsx
+++ src/InvoiceApp.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { getInvoice } from './services/getInvoice'
 import InvoiceView from './components/InvoiceView';
 import ClientView from './components/ClientView';
@@ -7,8 +7,21 @@ import ListItemView from './components/ListItemView';
 import TotalView from './components/TotalView';
 
 function InvoiceApp() {
-    const {invoice, total} = getInvoice();
-    console.log(invoice);
+    const { invoice, total } = getInvoice();
+
+    const [productValue, setProductValue] = useState('');
+    const [priceValue, setPriceValue] = useState('');
+    const [quantityValue, setQuantityValue] = useState('');
+
+    const [addItems, setAddItems] = useState(invoice.items);
+    const [counter, setCounter] = useState(addItems.length + 1);
+
+    //Auxiliares
+    const clearInputs = () => {
+        setProductValue('');
+        setPriceValue('');
+        setQuantityValue('');
+    }
 
     return (
         <>
@@ -30,8 +43,61 @@ function InvoiceApp() {
                                 <CompanyView title={'Datos de la empresa'} company={invoice.company}></CompanyView>
                             </div>
                         </div>
-                        <ListItemView title={'Productos de la Factura'} items={invoice.items}></ListItemView>
+                        <ListItemView title={'Productos de la Factura'} items={addItems}></ListItemView>
                         <TotalView total={total}></TotalView>
+
+                        <form onSubmit={event => {
+                            event.preventDefault();
+                            if(productValue.trim().length <=1) return;
+                            if(priceValue.trim().length <0) return;
+                            if(quantityValue.trim().length <0) return;
+
+                            setAddItems([...addItems,
+                            {
+                                id: counter,
+                                product: productValue,
+                                price: +priceValue,
+                                quantity: +quantityValue
+                            }]);
+                           
+                            clearInputs();
+                            setCounter(counter + 1);                          
+                        }}>
+                            <input type="text"
+                                name="product"
+                                value={productValue}
+                                placeholder="Producto"
+                                className="form-control m-4"
+                                onChange={event => {
+                                    console.log(event.target.value);
+                                    setProductValue(event.target.value);
+                                }} />
+
+                            <input type="text"
+                                name="price"
+                                value={priceValue}
+                                placeholder="Precio"
+                                className="form-control m-4"
+                                onChange={event => {
+                                    console.log(event.target.value);
+                                    setPriceValue(event.target.value);
+                                }} />
+
+                            <input type="text"
+                                name="quantity"
+                                value={quantityValue}
+                                placeholder="Cantidad"
+                                className="form-control m-4"
+                                onChange={event => {
+                                    console.log(event.target.value);
+                                    setQuantityValue(event.target.value);
+                                }} />
+
+                            <button 
+                                type="submit"
+                                className="btn btn-primary m-3">Nuevo Item</button>
+
+                        </form>
                     </div>
                 </div>
             </div>
